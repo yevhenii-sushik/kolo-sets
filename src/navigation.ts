3aafba37824f94dbe5db9be824d46ac6c5513342
Yevhenii <142432413+yevhenii-sushik@users.noreply.github.com>
2026-02-15 01:50:12 +0100

    bug fixes

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,10 +1,8 @@
 // navigation.ts
-import { Home, BookOpen, Edit3, HelpCircle, User } from 'lucide-react';
+import { Home, HelpCircle, User } from 'lucide-react';
 
 export const navItems = [
   { to: '/', label: 'Home', icon: Home },
-  // { to: '/collections', label: 'Sets', icon: BookOpen },
-  // { to: '/create', label: 'Create', icon: Edit3 },
   { to: '/about', label: 'About', icon: HelpCircle },
   { to: '/profile', label: 'Profile', icon: User },
 ];
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -6,7 +6,7 @@ export default function ProfilePage() {
     <div>
         <h1 className="text-center text-6xl font-bold mt-10">Coming soon</h1>
         <h3 className="text-center text-2xl font-bold mt-10">probably never.</h3>
-        <img src="public/moncey.jpg" alt="moncey" className="mx-auto mt-10 h-64" />
+        <img src="https://www.meme-arsenal.com/en/create/template/5818742" alt="monkey" className="mx-auto mt-10 h-64" />
 
         <p className="text-center text-xl mt-10">
             Profiles are on the way. Secure accounts and additional 
